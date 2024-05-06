44840751c7f0dffa26fd608aceb2fd877197d836
Cameron Devine <cameron.devine@alleninstitute.org>
2024-05-06 10:17:21 -0700

    modified scope message defs

--- TEM_comms/msgs/scope.py
+++ TEM_comms/msgs/scope.py
@@ -1,13 +1,23 @@
 from .base import BaseMessage
+from typing import Literal
+from pydantic import model_validator
 
 class command(BaseMessage):
     focus: int | None = None
-    aperture: int | None = None
+    aperture: Literal["lowmag", "highmag"] | None = None
+    mag_mode: Literal["LM", "MAG1", "MAG2"] | None = None
     mag: int | None = None
+
+    @model_validator(mode="after")
+    @classmethod
+    def check_mag(cls, data):
+        assert (data.mag_mode is None) == (data.mag is None)
+        return data
+
 
 class status(BaseMessage):
     focus: int
-    aperture: int
+    aperture: str | None
+    mag_mode: str
     mag: int
-    vacuum: float
     tank_voltage: int
