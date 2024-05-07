f712979592b76e2266cd059f0290aabf817aa694
Cameron Devine <cameron.devine@alleninstitute.org>
2024-05-06 17:55:48 -0700

    fixing the scope command message test

--- test/test_scope_command.py
+++ test/test_scope_command.py
@@ -1,21 +1,21 @@
-from TEM_comms.msgs.scope import command
+from TEM_comms.msgs.scope import Command
 from pydantic import ValidationError
 import pytest
 
 def test_mag():
     with pytest.raises(ValidationError):
-        command(mag_mode="LM")
+        Command(mag_mode="LM")
     
     with pytest.raises(ValidationError):
-        command(mag=10)
+        Command(mag=10)
 
-    command()
-    command(mag_mode="LM", mag=1)
+    Command()
+    Command(mag_mode="LM", mag=1)
 
 def test_mag_mode():
     with pytest.raises(ValidationError):
-        command(mag_mode="test", mag=1)
+        Command(mag_mode="test", mag=1)
 
-    command(mag_mode="LM", mag=1)
-    command(mag_mode="MAG2", mag=1)
-    command(mag_mode="MAG2", mag=1)
+    Command(mag_mode="LM", mag=1)
+    Command(mag_mode="MAG2", mag=1)
+    Command(mag_mode="MAG2", mag=1)
