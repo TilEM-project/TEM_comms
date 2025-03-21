a2fa833898294427709fe5492d69e55ae66459b3
Cameron Devine <cameron.devine@alleninstitute.org>
2025-03-21 00:38:19 -0700

    adding Z axis motion support

--- TEM_comms/stage/motion.py
+++ TEM_comms/stage/motion.py
@@ -4,11 +4,13 @@ from pigeon import BaseMessage
 class Command(BaseMessage):
     x: int | None = None
     y: int | None = None
+    z: int | None = None
     calibrate: bool = False
 
 
 class Status(BaseMessage):
     x: int | None
     y: int | None
+    z: int | None
     in_motion: bool
     error: str = ""
