684724907939ac83d608db291209f8cb2c3dea4f
Cameron Devine <cameron.devine@alleninstitute.org>
2025-09-11 10:40:58 -0700

    added documentation for the state messages

--- TEM_comms/state.py
+++ TEM_comms/state.py
@@ -1,10 +1,17 @@
 from pigeon import BaseMessage
+from pydantic import Field
 
 
 class Current(BaseMessage):
-    state: str
+    """
+    This message contains the current state of the orchastration state machine.
+    """
+    state: str = Field(description="The current state of the orchastration state machine.")
 
 
 class Change(BaseMessage):
-    previous: str
-    current: str
+    """
+    This message is sent on a state change of the orchastration state machine.
+    """
+    old: str = Field(description="The previous state of the orchastration state machine.")
+    new: str = Field(description="The new state of the orchastration state machine.")
